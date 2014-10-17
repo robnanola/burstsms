e3573def0014011a7d4dd67b3eb61924abf0f763
robnanola <robnanola@gmail.com>
2014-10-18 01:08:59 +0800

    added tests for transmit api

--- transmit_sms_api/api_client_v2.py
+++ transmit_sms_api/api_client_v2.py
@@ -1,17 +1,32 @@
 import base64
-import request
+import requests
+import json
 
 class Error(Exception):
     pass
 
 
 class TrasmitApiErrorError(Error):
+    """
+
+    AUTH_FAILED_NO_DATA 401     You have not provided auth data.
+    AUTH_FAILED         401     The auth data you have provided is invalid.
+    NOT_IMPLEMENTED     404     The method you are requesting is unsupported.
+    OVER_LIMIT          429     You have exceeded the request limit.
+    FIELD_EMPTY         400     Required field is empty.
+    FIELD_INVALID       400     Field has invalid format (see proper format in the description).
+    NO_ACCESS           400     You do not have access to this resource.
+    KEY_EXISTS          400     The resource with this key already exists.
+    NOT_FOUND           400     The resource with this key is not found.
+
+    """
+
     def __init__(self, code, message):
         Error.__init__(self, message)
         self.code = code
 
 
-class TransmitSmsApi(obj):
+class TransmitSmsApi(object):
 
     def __init__(self, api_key=None, api_secret=None):
 
@@ -23,26 +38,82 @@ class TransmitSmsApi(obj):
 
 
     def build_url_for(self, resource_name):
+        """
+        constructs to url with the given resource_name.
+        """
 
         return '%s/%s.json' % (self.api_url, resource_name)
 
 
-    def post(self, url, data={}):
+    def post(self, resource_name, data={}):
+        """
+        http post request to a given url and data.
+        """
+        url = self.build_url_for(resource_name)
 
-        resp = request.post(url, data, auth=(self.api_key, self.api_secret))
+        resp = requests.post(url, data, auth=(self.api_key, self.api_secret))
 
-        resp_dict = json.loads(resp.json())
-
-        print resp_dict
+        return resp.json()
 
 
     def send_sms(self, data={}):
 
-        url = self.build_url_for('send-sms')
+        """
 
-        self.post(url, data)
+        source: http://burst.transmitsms.com/api-docs/2/sms/send-sms
+
+        @data keys:
+            message - required
+            to - required if list_id is not set
+            from 
+            send_at
+            list_id - required if to is not set
+            dir_callback
+            reply_callback
+            validity
+            replies_to email
+
+
+        """
+
+        
+
+        return self.post('send-sms', data)
+
+
+    def get_sms(self, message_id):
+        """
+        Get information about a message you have sent.
+
+        """
+
+        self.post('get-sms', data={'message_id':message_id})
 
 
 
+    def get_sms_responses(self, data={}):
+        """
+        Pick up responses to messages you have sent. Filter by keyword or 
+        for just one phone number.
+
+        http://burst.transmitsms.com/api-docs/2/sms/get-sms-responses
 
 
+
+        """
+
+        self.post('get-sms-responses', data=data)
+
+
+    def get_sms_sent(self, data={}):
+        """
+        Get a list of recipients from a message send. 
+        Get up to date information such as opt-out status and delivery status.
+
+        http://burst.transmitsms.com/api-docs/2/sms/get-sms-sent
+        """
+
+        self.post('get-sms-sent', data=data)
+
+
+    
--- transmit_sms_api/tests/test_send_message.py
+++ transmit_sms_api/tests/test_send_message.py
@@ -6,18 +6,93 @@ import sys
 from api_client_v2 import TransmitSmsApi
 
 
+class TestSendSmsInvalidAuth(unittest.TestCase):
+
+    def setUp(self):
+
+        self.api = TransmitSmsApi('dcae6d3a62e8a88da9dd442fb1f08082','secretdaw1')
+
+
+    def test_send_sms_failed1(self):
+
+        data = {
+            'message':'test message',
+            'to':'6123456789',
+        }
+
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], u'AUTH_FAILED')
+
+
 class TestSendSms(unittest.TestCase):
 
-	def setUp(self):
+    def setUp(self):
 
-		self.api = TransmitSmsApi('dcae6d3a62e8a88da9dd442fb1f08082','secretdaw')
+        self.api = TransmitSmsApi('dcae6d3a62e8a88da9dd442fb1f08082','secretdaw')
 
 
-	def test_send_sms_failed1(self):
+    def test_send_sms_failed1(self):
 
-		data = {
-			'message':None
-			'to':
-		}
+        data = {
+            'message':None,
+            'to':'',
+        }
 
-		self.api.send_sms()
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], 'FIELD_EMPTY')
+
+
+    def test_send_sms_failed1(self):
+
+        data = {
+            'message':"test message",
+            'to':'',
+        }
+
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], 'FIELD_INVALID')
+        self.assertEqual(resp['error']['description'],u"You must provide either 'list_id' or 'to'" )
+
+
+    def test_send_sms_failed1(self):
+
+        data = {
+            'message':"",
+            'to':'1234456',
+        }
+
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], 'FIELD_EMPTY')
+        self.assertEqual(resp['error']['description'],u"Required field 'message' is empty" )
+
+
+
+    def test_send_sms_failed1(self):
+
+        data = {
+            'message':"test message 2",
+            'to':'1234456',
+        }
+
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], 'FIELD_INVALID')
+        self.assertEqual(resp['error']['description'],u'Field "to" is not a valid number.' )
+
+    def test_send_sms_valid(self):
+
+        data = {
+            'message':"test message valid",
+            'to':'61422265404',
+        }        
+
+
+        resp = self.api.send_sms(data)
+
+        self.assertEqual(resp['error']['code'], 'SUCCESS')
+        self.assertEqual(resp['error']['description'],u'OK' )
+
